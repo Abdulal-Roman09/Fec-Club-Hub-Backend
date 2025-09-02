64d69cd2dd9a92a20bd2d07d4e5af66389fa50f5
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-03 01:54:42 +0600

    add all clue model

--- models/club.js
+++ models/club.js
@@ -1,2 +1,56 @@
+import mongoose from "mongoose";
 
-const ClubSchema=new mongoose.Sch
+const ClubSchema = new mongoose.Schema(
+  {
+    clubName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    clubId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    clubSortName: {
+      type: String,
+      required: true,
+    },
+    clubMotto: {
+      type: String,
+      required: true,
+    },
+    clubDescription: {
+      type: String,
+      required: true,
+    },
+    clubLogo: {
+      type: String,
+      required: true,
+    },
+    clubBanner: {
+      type: String,
+      required: true,
+    },
+    clubCategory: {
+      type: String,
+      required: true,
+      enum: [
+        "Innovation",
+        "Technology",
+        "Arts",
+        "Religious",
+        "Community",
+        "Sports",
+        "Creative",
+        "Academic",
+        "Cultural",
+        "Professional",
+        "Education",
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Club", ClubSchema);
